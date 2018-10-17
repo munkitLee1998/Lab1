da4b4d6c85945ad6b622dce8a5fb777c0ffbffba
anreh <anurehka@gmail.com>
2018-10-17 13:16:04 +0800

    update 2

--- app/src/main/java/com/example/taruc/lab1/MainActivity.java
+++ app/src/main/java/com/example/taruc/lab1/MainActivity.java
@@ -13,7 +13,9 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
